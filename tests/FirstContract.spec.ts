f6550edcedd344ce9503ddc43de1d0cf4c9d9288
Alina Perevozchikova <alinka.12.12.98@gmail.com>
2024-05-09 11:21:35 +0400

    feat: create first contract Counter and tests for it

---
+++ tests/FirstContract.spec.ts
@@ -0,0 +1,59 @@
+import { toNano } from '@ton/core';
+import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
+import '@ton/test-utils';
+import { FirstContract } from '../wrappers/FirstContract';
+
+describe('FirstContract', () => {
+    let blockchain: Blockchain;
+    let deployer: SandboxContract<TreasuryContract>;
+    let firstContract: SandboxContract<FirstContract>;
+
+    beforeEach(async () => {
+        blockchain = await Blockchain.create();
+
+        firstContract = blockchain.openContract(await FirstContract.fromInit());
+
+        deployer = await blockchain.treasury('deployer');
+
+        const deployResult = await firstContract.send(
+            deployer.getSender(),
+            {
+                value: toNano('0.05'),
+            },
+            {
+                $$type: 'Deploy',
+                queryId: 0n,
+            }
+        );
+
+        expect(deployResult.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: firstContract.address,
+            deploy: true,
+            success: true,
+        });
+    });
+
+    it('should deploy', async () => {
+        // the check is done inside beforeEach
+        // blockchain and firstContract are ready to use
+    });
+
+    it('should increase', async () => {
+        const counterBefore = await firstContract.getCounter()
+        console.log('counterBefore', counterBefore)
+
+        await firstContract.send(
+            deployer.getSender(),
+            {
+                value: toNano('0.02')
+            },
+            'increment'
+        )
+
+        const counterAfter = await firstContract.getCounter();
+        console.log('counterAfter', counterAfter)
+
+        expect(counterBefore).toBeLessThan(counterAfter)
+    });
+});
